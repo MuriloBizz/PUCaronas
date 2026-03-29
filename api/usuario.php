7c8e71c1d4f5989bae0d741f24403d76e00072b9
Gustavo <gustavo.boganika@gmail.com>
2026-03-29 17:26:28 -0300

    Desenvolvimento API de usuário e CRUD

---
+++ api/usuario.php
@@ -0,0 +1,99 @@
+<?php
+include '../../config/db.php';
+
+header("Content-Type: application/json");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'), true);
+$action = $_GET['action'] ?? '';
+
+
+switch ($method) {
+    case 'GET':
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $result = $conn->query("SELECT * FROM usuario WHERE id=$id");
+            $data = $result->fetch_assoc();
+            echo json_encode($data);
+        }
+        elseif (isset($_GET['email'])) { 
+        $email = $_GET['email'];
+        $result = $conn->query("SELECT * FROM usuario WHERE email='$email'");
+        $data = $result->fetch_assoc();
+        echo json_encode($data);}
+        
+        else {
+            $result = $conn->query("SELECT * FROM usuario");
+            $users = [];
+            while ($row = $result->fetch_assoc()) {
+                $users[] = $row;
+            }
+            echo json_encode($users);
+        }
+        break;
+
+    case 'POST':
+        switch ($action){
+        case 'cadastro':
+        $nome = $input['nome'];
+        $email = $input['email'];
+        $senha_hash = $input['senha_hash'];
+        $data_nasc = $input['data_nasc'];
+        $status = "espera";
+        $cargo = $input['cargo'];
+        $doc = $input['doc'];
+        if ($conn->query("INSERT INTO usuario (nome, email,senha_hash,data_nasc,status,cargo,doc) VALUES ('$nome', '$email', '$senha_hash', '$data_nasc', '$status', '$cargo','$doc')")){
+            echo json_encode(["message" => "Usuário adicionado com sucesso."]);
+        }
+        else{
+             echo json_encode(["error" => $conn->error]);
+        }
+            break;
+        
+        case 'login':
+
+                $email = $input['email'];
+                $senha = $input['senha_hash'];
+                $result = $conn->query("SELECT * FROM usuario WHERE email='$email'");
+                $user = $result->fetch_assoc();
+                if($user && $user['senha_hash'] === $senha) {
+                    echo json_encode(["message" => "Login ok", "nome" => $user['nome']]);
+                } else {
+                    echo json_encode(["error" => "Email ou senha inválidos"]);
+                }
+                break;
+
+        
+        
+        }
+        break;
+
+    case 'PUT':
+        $id = $_GET['id'];
+        $nome = $input['nome'];
+        $email = $input['email'];
+        $senha_hash = $input['senha_hash'];
+        $data_nasc = $input['data_nasc'];
+        $status = $input['status'];
+        $cargo = $input['cargo'];
+        $doc = $input['doc'];
+        if  (  $conn->query("UPDATE usuario SET nome='$nome' ,email='$email',senha_hash='$senha_hash' ,data_nasc = '$data_nasc', status ='$status' ,cargo = '$cargo' ,doc = '$doc' WHERE id=$id")
+       ) {echo json_encode(["message" => "Usuário atualizado com sucesso"]);}
+       else{
+             echo json_encode(["error" => $conn->error]);
+       } 
+        break;
+
+    case 'DELETE':
+        $id = $_GET['id'];
+        $conn->query("DELETE FROM usuario WHERE id=$id");
+        echo json_encode(["message" => "Usuário deletado com sucesso"]);
+        break;
+
+    default:
+        echo json_encode(["message" => "Método Inválido"]);
+        break;
+}
+
+$conn->close();
+?>
